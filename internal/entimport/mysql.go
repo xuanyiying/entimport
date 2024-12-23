136dd8c186601da0f69329956d5eb36243f99c18
xuanyiying <1148064128@qq.com>
2024-12-23 15:11:08 +0800

    bigint -> int64

--- internal/entimport/mysql.go
+++ internal/entimport/mysql.go
@@ -124,7 +124,7 @@ func (m *MySQL) convertInteger(typ *schema.IntegerType, name string) (f ent.Fiel
 		f = field.Int32(name)
 	case mBigInt:
 		// Int64 is not used on purpose.
-		f = field.Int(name)
+		f = field.Int64(name)
 	}
 	return f
 }
